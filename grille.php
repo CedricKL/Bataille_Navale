f50de3f010a1706f909c5c5ed284118fce1b88a3
Cédric Leonel <cedricleonelkouam@gmail.com>
2023-02-27 18:05:44 +0100

    positionnement des bateaux (statiquement)

--- grille.php
+++ grille.php
@@ -41,6 +41,15 @@
                 } 
             ?>     
         </div>
+        <div class="choix">
+            <?php 
+                for($i=0;$i<40;$i++){
+                    if($i % 10 == 0){
+                        
+                    }
+                } 
+            ?>
+        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
---
+++ js/navire.js
@@ -0,0 +1,11 @@
+function creerNavire(){
+    for(let i=2;i<=6;i++){
+        $("#j"+i+"0").css("background-color","blue");
+        $("#j"+i).css("background-color","blue");
+        $("#j"+(i+43)).css("background-color","blue");
+        $("#j"+(i+81)).css("background-color","blue");
+    }
+        
+
+    
+}
