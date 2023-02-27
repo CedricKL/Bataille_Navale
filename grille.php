17eb2108ee0bac4253f53419afd27d634f5c02bf
Gilles Bruel Youmbou Ouachi <youmboug@3il>
2023-02-27 18:02:38 +0100

    ajout du feu au carreau apres un click

--- grille.php
+++ grille.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="css/grille.css">
+    <script src="js/jquery-3.6.3.min.js"></script>
+    <script src="js/joueur.js"></script>
     <title>Grille</title>
 </head>
 <body class="container">
@@ -28,14 +30,14 @@
         <div class="grid">
             <?php 
                 for($i=0;$i<100;$i++){
-                    echo "<div class=\"carreau\" id=\"c$i\"></div>";
+                    echo "<div class=\"carreau\" id=\"j$i\"></div>";
                 } 
             ?>     
         </div>
         <div class="grid">
             <?php 
                 for($i=0;$i<100;$i++){
-                    echo "<div class=\"carreau\" id=\"c$i\"></div>";
+                    echo "<div class=\"carreau carreauEnnemi \" id=\"e$i\" onclick=\"gererClick($i)\"></div>";
                 } 
             ?>     
         </div>
---
+++ js/joueur.js
@@ -0,0 +1,13 @@
+function gererClick(id) {
+    // console.log("la case a été cliquée"+ e.id);
+    //console.log($(this));
+     console.log(id);
+
+   //  $("#j"+id).html("<img src=\"img/feu.gif\" alt=\"feu\" width=\"15\" height=\"15\">");
+   $("#e"+id).css("background-image", "url(\"img/feu.gif\")");
+   $("#e"+id).css("background-size", "contain");
+   $("#e"+id).css("background-repeat", "no-repeat");
+}
+
+
+
